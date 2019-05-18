8bbf630c38a2d09f1c805d375e51ee84bba1a585
dsi28 <dsi28@hotmail.com>
2019-05-18 07:15:01 -0700

    create /models/request.js. update er diagram

---
+++ models/Request.js
@@ -0,0 +1,14 @@
+module.exports = function(sequelize,DataTypes){
+    return sequelize.define('Request',{
+        requestId: {
+            type: DataTypes.INTEGER,
+            primaryKey: true,
+            autoIncrement: true
+        },
+        status: { //TABLE ROW
+           type: DataTypes.STRING, //EXPECTING A STRING {COLUMN STRUCTURE}
+           AllowNull: false, //THIS COLUMN CANNOT BE EMPTY
+           defaultValue: 'pending'
+        }
+    });
+}
